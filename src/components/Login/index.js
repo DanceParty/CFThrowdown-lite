90a4f4f5415bf66b3d867f3606440dfba8e35561
jkd65 <jkd65@nau.edu>
2017-11-30 18:59:52 -0700

    Reformatted login page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Button, Text, TextInput, View } from 'react-native'
+import { Card } from 'react-native-elements'
 
 // styles
-import { container } from '../../styles/container'
-import { textInput } from '../../styles/textInput'
+import { typography } from '../../styles/typography'
+import { card } from '../../styles/card'
+import { form } from '../../styles/form'
 
 
 class Login extends React.Component {
+
+  state = {
+    textInputFocused: undefined
+  }
 
   handleLogin = () => {
     this.props.handleLogin()
@@ -20,34 +26,71 @@ class Login extends React.Component {
     this.props.handleChangePassword(text)
   }
 
+  handleTextInputFocus = (textInput) => {
+    this.setState({
+      textInputFocused: textInput,
+    })
+  }
+
+  handleTextInputOff = () => {
+    this.setState({
+      textInputFocused: undefined,
+    })
+  }
+
   render() {
     const email = this.props.email
     const password = this.props.password
+    const focusedInput = this.state.textInputFocused
     return (
-      <View style={[container.container, { alignItems: 'center', justifyContent: 'center' }]}>
-        <View style={[textInput.containerFull]}>
-          <TextInput
-            style={textInput.textInput}
-            placeholder="Email..."
-            underlineColorAndroid="transparent"
-            onChangeText={(text) => this.handleChangeEmail(text)}
-            value={email}
-          />
-          <TextInput
-            style={textInput.textInput}
-            placeholder="Password..."
-            underlineColorAndroid="transparent"
-            onChangeText={(text) => this.handleChangePassword(text)}
-            value={password}
-            secureTextEntry
-          />
-          <Button
-            onPress={this.handleLogin}
-            title="Login"
-            color="purple"
-            accessibilityLabel="Login"
-          />
-        </View>
+      <View>
+        <Card containerStyle={{ padding: 0 }}>
+
+          <View style={card.header}>
+            <Text style={[typography.title1, card.title]}>Login</Text>
+          </View>
+
+          <View style={card.unalignedContent}>
+            <View style={form.row}>
+              <View style={form.container}>
+                <Text style={typography.footnote}>Email</Text>
+                <TextInput
+                  style={(focusedInput === 'email') ? form.focusedTextInput : form.textInput}
+                  underlineColorAndroid="transparent"
+                  onFocus={() => this.handleTextInputFocus('email')}
+                  onEndEditing={() => this.handleTextInputOff()}
+                  onChangeText={(text) => this.handleChangeEmail(text)}
+                  value={email}
+                />
+              </View>
+            </View>
+            <View style={form.row}>
+              <View style={form.container}>
+                <Text style={typography.footnote}>Password</Text>
+                <TextInput
+                  style={(focusedInput === 'password') ? form.focusedTextInput : form.textInput}
+                  underlineColorAndroid="transparent"
+                  onFocus={() => this.handleTextInputFocus('password')}
+                  onEndEditing={() => this.handleTextInputOff()}
+                  onChangeText={(text) => this.handleChangePassword(text)}
+                  value={password}
+                  secureTextEntry
+                />
+              </View>
+            </View>
+            <View style={[form.row, { justifyContent: 'center', paddingBottom: '10%' }]}>
+              <View style={form.container}>
+              <Button
+                onPress={this.handleLogin}
+                title="Login"
+                color="#4492D0"
+                accessibilityLabel="Login"
+              />
+              </View>
+            </View>
+          </View>
+
+        </Card>
       </View>
     )
   }
